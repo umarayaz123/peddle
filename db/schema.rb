c2a0bed3c55968a346e1d2b70bb31aa0c914772d
rac <rac@ubuntu.ubuntu-domain>
2011-10-27 12:16:02 -0400

    todays changes commit

--- db/schema.rb
+++ db/schema.rb
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(:version => 20111020220003) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "store_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
