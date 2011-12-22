f325225e375d49cf664188ff8cf20d39558b701d
Adnan Ali Arshad <adnan@adnan.(none)>
2011-12-22 11:25:22 +0500

    changes ...

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,17 +2,17 @@ class HomeController < ApplicationController
   layout "application"
 
   def index
-    @stores = Store.limit(3).offset(0)
+    @stores          = Store.limit(3).offset(0)
     @featured_stores = Store.where("is_featured = 1")
     unless current_user
       @featured_stores = Store.where("is_featured = 1")
-      @stores = @featured_stores.limit(4).offset(0)
+      @stores          = @featured_stores.limit(4).offset(0)
       render :action => "home"
     end
   end
 
   def next_stores
-    offset = params[:offset]
+    offset  = params[:offset]
     @stores = Store.limit(3).offset(offset.to_i)
     render :layout => false
   end
@@ -31,13 +31,13 @@ class HomeController < ApplicationController
   end
 
   def plans
-    @package_rules =Package.all
-    @allowed_products = PackageRule.where(:key => "allowed_products")
+    @package_rules         =Package.all
+    @allowed_products      = PackageRule.where(:key => "allowed_products")
     @allowed_staff_members = PackageRule.where(:key => "allowed_staff_members")
-    @allowed_images = PackageRule.where(:key => "allowed_images")
-    @chat = PackageRule.where(:key => "chat")
-    @inventory_tracking = PackageRule.where(:key => "inventory_tracking")
-    @discount_count = PackageRule.where(:key => "discount_count")
+    @allowed_images        = PackageRule.where(:key => "allowed_images")
+    @chat                  = PackageRule.where(:key => "chat")
+    @inventory_tracking    = PackageRule.where(:key => "inventory_tracking")
+    @discount_count        = PackageRule.where(:key => "discount_count")
     render :layout => false
   end
 
@@ -50,11 +50,13 @@ class HomeController < ApplicationController
   end
 
   def sales_floor
+    #@featured_stores = Store.where("is_featured = 1")
+    #@stores = @featured_stores.limit(4).offset(0)
+    @stores          = Store.limit(3).offset(0)
     @featured_stores = Store.where("is_featured = 1")
-    @stores = @featured_stores.limit(4).offset(0)
     unless current_user
       @featured_stores = Store.where("is_featured = 1")
-      @stores = @featured_stores.limit(4).offset(0)
+      @stores          = @featured_stores.limit(4).offset(0)
       render :action => "home"
       return
     end
@@ -63,7 +65,7 @@ class HomeController < ApplicationController
 
   def check_store
     @store = Store.find_by_name(params[:store_name])
-    exist =false
+    exist  =false
     unless @store.nil?
       exist =true
     end
