a696136b8d97fe7cb277c2618a6e07a233e31bad
Adnan Ali Arshad <adnan@adnan.(none)>
2011-12-22 18:19:43 +0500

    changes ....

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -111,14 +111,14 @@ class Users::RegistrationsController < ApplicationController
 
   # GET /resource/edit
   def edit
-    #@request_type = "plain"
-    #if request.xhr?
-    #  @request_type = "ajax"
-    #  render :layout => false
-    #  puts "************ request.xhr true", request.xhr?
-    #  return
-    #end
-    #puts "************ request.xhr FALSE", request.xhr?
+    @request_type = "plain"
+    if request.xhr?
+      @request_type = "ajax"
+      render :layout => false
+      puts "************ request.xhr true", request.xhr?
+      return
+    end
+    puts "************ request.xhr FALSE", request.xhr?
     render_with_scope :edit
   end
 
@@ -151,46 +151,42 @@ class Users::RegistrationsController < ApplicationController
         end
       end
     end
-    user = current_user
-    if user.valid_password?(params[:user][:current_password])
+    @user = current_user
+    if @user.update_with_password(params[:user])
       @store.update_attribute(:package_id, package.id) unless current_user.roles.include?(buyer_role)
-      self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-      if resource.update_with_password(params[resource_name])
-        unless @order.nil?
-          response = @order.capture_store_package(@price, current_user.id, @authorise)
-          if response.success?
-            @order.update_attributes!(:user_id => current_user.id)
-            @store.update_attribute(:package_id, package.id)
-          end
+      unless @order.nil?
+        response = @order.capture_store_package(@price, current_user.id, @authorise)
+        if response.success?
+          @order.update_attributes!(:user_id => current_user.id)
+          @store.update_attribute(:package_id, package.id)
         end
-        set_flash_message :notice, :updated if is_navigational_format?
-        sign_in resource_name, resource, :bypass => true
-        #@request_type = "plain"
-        #if request.xhr?
-        #  @request_type = "ajax"
-        #  puts "************ request.xhr true updated value", request.xhr?
-        #  render :template =>"/index_layout", :layout => false
-        #  return
-        #end
-        respond_with resource, :location => after_update_path_for(resource)
-      else
-        clean_up_passwords(resource)
-        respond_with_navigational(resource) { render_with_scope :edit }
       end
+      set_flash_message :notice, :updated if is_navigational_format?
+      sign_in resource_name, resource, :bypass => true
+      @request_type = "plain"
+      if request.xhr?
+        @request_type = "ajax"
+        redirect_to root_url+"index_layout"
+        return
+      end
+      respond_with resource, :location => after_update_path_for(resource)
+
     else
-      #@request_type = "plain"
-      #if request.xhr?
-      #  @request_type = "ajax"
-      #  puts "************ request.xhr true update", request.xhr?
-      #  render :action => "edit", :layout => false
-      #  return
-      #end
-      clean_up_passwords(resource)
-      respond_with_navigational(resource) { render_with_scope :edit }
+      @request_type = "plain"
+      if request.xhr?
+        puts '**********in here'
+        @request_type = "ajax"
+        #render :action => "edit", :layout => false
+        #
+        #return
+      end
+      render :text => "update_failed"
+      #clean_up_passwords(resource)
+      #respond_with_navigational(resource) { render_with_scope :edit }
     end
   end
 
-  # DELETE /resource
+# DELETE /resource
   def destroy
     resource.destroy
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
@@ -198,11 +194,11 @@ class Users::RegistrationsController < ApplicationController
     respond_with_navigational(resource) { redirect_to after_sign_out_path_for(resource_name) }
   end
 
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
+# GET /resource/cancel
+# Forces the session data which is usually expired after sign
+# in to be expired now. This is useful if the user wants to
+# cancel oauth signing in/up in the middle of the process,
+# removing all OAuth session data.
   def cancel
     expire_session_data_after_sign_in!
     redirect_to new_registration_path(resource_name)
@@ -210,38 +206,38 @@ class Users::RegistrationsController < ApplicationController
 
   protected
 
-  # Build a devise resource passing in the session. Useful to move
-  # temporary session data to the newly created user.
+# Build a devise resource passing in the session. Useful to move
+# temporary session data to the newly created user.
   def build_resource(hash=nil)
     hash          ||= params[resource_name] || { }
     self.resource = resource_class.new_with_session(hash, session)
   end
 
-  # The path used after sign up. You need to overwrite this method
-  # in your own RegistrationsController.
+# The path used after sign up. You need to overwrite this method
+# in your own RegistrationsController.
   def after_sign_up_path_for(resource)
     after_sign_in_path_for(resource)
   end
 
-  # Overwrite redirect_for_sign_in so it takes uses after_sign_up_path_for.
+# Overwrite redirect_for_sign_in so it takes uses after_sign_up_path_for.
   def redirect_location(scope, resource)
     stored_location_for(scope) || after_sign_up_path_for(resource)
   end
 
-  # Returns the inactive reason translated.
+# Returns the inactive reason translated.
   def inactive_reason(resource)
     reason = resource.inactive_message.to_s
     I18n.t("devise.registrations.reasons.#{reason}", :default => reason)
   end
 
-  # The path used after sign up for inactive accounts. You need to overwrite
-  # this method in your own RegistrationsController.
+# The path used after sign up for inactive accounts. You need to overwrite
+# this method in your own RegistrationsController.
   def after_inactive_sign_up_path_for(resource)
     root_path
   end
 
-  # The default url to be used after updating a resource. You need to overwrite
-  # this method in your own RegistrationsController.
+# The default url to be used after updating a resource. You need to overwrite
+# this method in your own RegistrationsController.
   def after_update_path_for(resource)
     if defined?(super)
       ActiveSupport::Deprecation.warn "Defining after_update_path_for in ApplicationController " <<
@@ -252,7 +248,7 @@ class Users::RegistrationsController < ApplicationController
     end
   end
 
-  # Authenticates the current scope and gets the current resource from the session.
+# Authenticates the current scope and gets the current resource from the session.
   def authenticate_scope!
     send(:"authenticate_#{resource_name}!", :force => true)
     self.resource = send(:"current_#{resource_name}")
